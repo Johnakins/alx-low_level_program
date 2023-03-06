b31dab1e61332e57c807e9e567cade6fda8f8b00
Johnakins <johnolawole1@gmail.com>
2023-03-06 09:57:44 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -13,7 +13,7 @@ void simple_print_buffer(char *buffer, unsigned int size)
 	unsigned int i;
 
 	i = 0;
-        while (i < size)
+	while (i < size)
 	{
 		if (i % 10)
 		{
@@ -26,4 +26,19 @@ void simple_print_buffer(char *buffer, unsigned int size)
 		printf("0x%02x", buffer[i]);
 		i++;
 	}
+}
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char buffer[98] = {0x00};
+
+	simple_print_buffer(buffer, 98);
+	_memset(buffer, 0x01, 95);
+	printf("-------------------------------------------------\n");
+	simple_print_buffer(buffer, 98);
+	return (0);
 }
