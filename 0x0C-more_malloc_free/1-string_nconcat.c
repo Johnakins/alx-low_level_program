64fe25e1e68f32e40f73024c505dfc68d81ab265
Johnakins <johnolawole1@gmail.com>
2023-03-16 14:36:07 -0700

    1-malloc.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -36,7 +36,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; i < lsout; i++)
 		if (i < ls1)
 			sout[i] = s1[i];
-		else 
+		else
 			sout[i] = s2[i - ls1];
 	sout[i] = '\0';
 	return (sout);
